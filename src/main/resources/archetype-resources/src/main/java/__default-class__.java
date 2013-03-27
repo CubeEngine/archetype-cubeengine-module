2b88a6e9c9e6e61bad77e876620bbe63c79d1eb6
Phillip Schichtel <quick_wango@code-infection.de>
2013-03-27 22:41:20 +0100

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/__default-class__.java
@@ -0,0 +1,8 @@
+package ${package};
+
+import de.cubeisland.cubeengine.core.module.Module;
+
+public class ${default-class} extends Module
+{
+    private ${default-class}Config config;
+}
---
+++ src/main/resources/archetype-resources/src/main/java/__default-class__Config.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import de.cubeisland.cubeengine.core.config.Configuration;
+import de.cubeisland.cubeengine.core.config.annotations.Codec;
+import de.cubeisland.cubeengine.core.config.annotations.DefaultConfig;
+import de.cubeisland.cubeengine.core.config.annotations.Option;
+
+@Codec("yml")
+@DefaultConfig
+public class ${default-class}Config extends Configuration
+{}
