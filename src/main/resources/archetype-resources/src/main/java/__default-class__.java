1057c3d6808503d0a795ba2faf79803d1459a4cd
Faithcaio <Faithcaio@yahoo.de>
2013-08-11 05:52:32 +0200

    Update __default-class__.java

--- src/main/resources/archetype-resources/src/main/java/__default-class__.java
+++ src/main/resources/archetype-resources/src/main/java/__default-class__.java
@@ -5,4 +5,10 @@ import de.cubeisland.engine.core.module.Module;
 public class ${default-class} extends Module
 {
     private ${default-class}Config config;
+    
+    @Override
+    public void onEnable()
+    {
+        this.config = Configuration.load(${default-class}Config.class, this);
+    }
 }
