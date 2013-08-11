ec11f3b81ba982846b60b96718e593e392278908
Faithcaio <Faithcaio@yahoo.de>
2013-08-11 05:44:50 +0200

    Update __default-class__Config.java

--- src/main/resources/archetype-resources/src/main/java/__default-class__Config.java
+++ src/main/resources/archetype-resources/src/main/java/__default-class__Config.java
@@ -1,11 +1,6 @@
 package ${package};
 
-import de.cubeisland.cubeengine.core.config.Configuration;
-import de.cubeisland.cubeengine.core.config.annotations.Codec;
-import de.cubeisland.cubeengine.core.config.annotations.DefaultConfig;
-import de.cubeisland.cubeengine.core.config.annotations.Option;
+import de.cubeisland.engine.core.config.YamlConfiguration;
 
-@Codec("yml")
-@DefaultConfig
-public class ${default-class}Config extends Configuration
+public class ${default-class}Config extends YamlConfiguration
 {}
