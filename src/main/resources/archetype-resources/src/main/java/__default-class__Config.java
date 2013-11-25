5b847bdfa5b6056c3c14bd8e8fdccda2fc15ff1b
Faithcaio <Faithcaio@users.noreply.github.com>
2013-11-25 22:27:43 +0100

    Update __default-class__Config.java

--- src/main/resources/archetype-resources/src/main/java/__default-class__Config.java
+++ src/main/resources/archetype-resources/src/main/java/__default-class__Config.java
@@ -1,6 +1,6 @@
 package ${package};
 
-import de.cubeisland.engine.core.config.YamlConfiguration;
+import de.cubeisland.engine.configuration.YamlConfiguration;
 
 public class ${default-class}Config extends YamlConfiguration
 {}
