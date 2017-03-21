71955a71bd2d84e9947f2b259942cc1bace0b935
Faithcaio <Faithcaio@users.noreply.github.com>
2017-03-21 17:57:04 +0100

    Update __default-class__.java

--- src/main/resources/archetype-resources/src/main/java/__default-class__.java
+++ src/main/resources/archetype-resources/src/main/java/__default-class__.java
@@ -1,24 +1,8 @@
 package ${package};
 
-import java.nio.file.Path;
-import javax.inject.Inject;
-import de.cubeisland.engine.logscribe.Log;
 import de.cubeisland.engine.modularity.asm.marker.ModuleInfo;
 import de.cubeisland.engine.modularity.core.Module;
-import de.cubeisland.engine.modularity.core.marker.Disable;
 import de.cubeisland.engine.modularity.core.marker.Enable;
-import org.cubeengine.reflect.Reflector;
-import org.cubeengine.libcube.service.permission.Permission;
-import org.cubeengine.module.shout.announce.Announcement;
-import org.cubeengine.module.shout.announce.AnnouncementManager;
-import org.cubeengine.module.shout.interactions.ShoutCommand;
-import org.cubeengine.module.shout.interactions.ShoutListener;
-import org.cubeengine.libcube.service.command.CommandManager;
-import org.cubeengine.libcube.service.event.EventManager;
-import org.cubeengine.libcube.service.i18n.I18n;
-import org.cubeengine.libcube.service.matcher.StringMatcher;
-import org.cubeengine.libcube.service.permission.PermissionManager;
-import org.cubeengine.libcube.service.task.TaskManager;
 import org.cubeengine.libcube.service.filesystem.ModuleConfig;
 
 @ModuleInfo(name = "${default-class}", description = "${description}")
